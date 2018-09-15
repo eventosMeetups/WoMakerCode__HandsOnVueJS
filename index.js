6e0020eddb077392ce52a544ed655b981e4cb15a
Mara <mara.15oliveira@outlook.com>
2018-09-15 10:39:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+/*
+HELPS COM GIT
+
+https://services.github.com/on-demand/downloads/pt_BR/github-git-cheat-sheet/
+
+VUE HELP
+
+https://github.com/ksetoue/vue-fullstackapp
+
+MONGOLAB - BD PRA PEQUENAS APLICAOES DO MONGO
+
+*/
