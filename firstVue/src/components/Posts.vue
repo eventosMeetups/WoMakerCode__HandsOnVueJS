02dc692745b86689803a352141e12080ed74c736
Mara <mara.15oliveira@outlook.com>
2018-09-15 16:54:28 -0300

    Alt

--- firstVue/src/components/Posts.vue
+++ firstVue/src/components/Posts.vue
@@ -2,12 +2,33 @@
   <div class="posts">
     <h1>Posts</h1>
     This file will list all the posts.
-
     <div v-for="post in postsList" :key="post._id">
       <p>
         <span v-text = "post.user"></span>
         <span v-text = "post.content"></span>
       </p>
+
+        <div class="card">
+          <b-card title="Card title"
+                  sub-title="Card subtitle">
+
+              <h5 class="mt-0" v-text="post.user" ></h5>
+              <p v-text="post.content"></p>
+
+              <p class="card-text" >
+                  Some quick example text to build on the <em>card title</em> and make up the bulk of the card's content.
+              </p>
+              <a href="#"
+                class="card-link">Card link</a>
+              <b-link href="#"
+                      class="card-link">Another link</b-link>
+         
+              <div class="buttom">
+                <b-button>Remover</b-button>
+                <b-button href="#">I am a Link</b-button>
+              </div>
+          </b-card>
+        </div>
     </div>
   </div>
 </template>
--- firstVue/src/main.js
+++ firstVue/src/main.js
@@ -6,8 +6,11 @@ import router from './router'
 import Posts from '@/components/Posts'
 import Router from 'vue-router'
 import Store from './store/store'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
 
 export default new Router({
 
